2e7f1717c9bc26347560592fd2ed901759e89bc3
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-30 17:42:02 -0300

    deploy products in store

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .import models
 
 # Create your views here.
+
+def store(request):
+    products = models.Product.objects.all().filter(is_available = True)
+    product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count
+    }
+    return render(request , 'store/store.html' , context)
