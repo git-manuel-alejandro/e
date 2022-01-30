8d7489f424effa32664cd48b5a016181e044863b
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-30 16:20:36 -0300

    setting admin accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .import models
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .import models
 
-admin.site.register(models.Account)
+class AccountAdmin(UserAdmin):
+    list_display = ['email','first_name', 'last_name', 'username', 'last_login', 'is_active']
+    list_display_link = ('email','first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined', )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(models.Account , AccountAdmin)
+
