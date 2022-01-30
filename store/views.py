9e5405c1d83e3ab881764aa226c655c1224d338f
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-30 17:51:22 -0300

    search products by category

--- store/views.py
+++ store/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from unicodedata import category
+from django.shortcuts import get_object_or_404, render
 from .import models
+from category.models import Category
 
 # Create your views here.
 
-def store(request):
-    products = models.Product.objects.all().filter(is_available = True)
-    product_count = products.count()
+def store(request , category_slug = None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category , slug =  category_slug)
+        products = models.Product.objects.filter(category = categories , is_available = True)
+        product_count = products.count()
+    else:
+        products = models.Product.objects.all().filter(is_available = True)
+        product_count = products.count()
+    
     context = {
         'products' : products,
         'product_count' : product_count
